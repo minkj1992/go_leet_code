e5c4bcd46b2ad4889b42e8b67b26b6fff8c29304
minkj1992 <minkj1992@gmail.com>
2020-11-22 21:54:14 +0900

    Merge Two Sorted List

---
+++ easy/mergeTwoSortedLists.go
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// recursive
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists(l1, l2.Next)
+	return l2
+}
+
+// non recursive
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	res := &ListNode{}
+	cur := res
+	for l1 != nil || l2 != nil {
+		if l1 == nil {
+			cur.Next = l2
+			break
+		}
+		if l2 == nil {
+			cur.Next = l1
+			break
+		}
+		if l1.Val > l2.Val {
+			cur.Next = l2
+			cur, l2 = cur.Next, l2.Next
+		} else {
+			cur.Next = l1
+			cur, l1 = cur.Next, l1.Next
+		}
+	}
+	return res.Next
+}
